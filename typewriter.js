4b34a0a0397e26e42dcdfe42f013460fe50534e9
George Tang <georgecctang@gmail.com>
2020-09-22 17:38:24 +0000

    added interval variable

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,9 @@
 const sentence = "hello there from lighthouse labs";
 
 let delay = 0;
+let interval = 50;
 for (let char of sentence) {
   setTimeout(() => process.stdout.write(char), delay);
-  delay += 50;
+  delay += interval;
 }
 setTimeout(() => process.stdout.write('\n'), delay);
